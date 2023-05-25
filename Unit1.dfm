58fe29478efebdd4b1b35166ee129e019544d8c7
Ivanov Dmitriy <dig.ivanov09@gmail.com>
2023-05-26 00:45:46 +0300

    add save to binary file

--- Unit1.dfm
+++ Unit1.dfm
@@ -1,10 +1,10 @@
 object Form1: TForm1
-  Left = 942
-  Top = 436
+  Left = 279
+  Top = 337
   BorderIcons = [biSystemMenu, biMinimize]
   BorderStyle = bsSingle
   Caption = 'RGB565/BGR565 converter'
-  ClientHeight = 365
+  ClientHeight = 370
   ClientWidth = 795
   Color = clGrayText
   Font.Charset = DEFAULT_CHARSET
@@ -12,6 +12,7 @@ object Form1: TForm1
   Font.Height = -11
   Font.Name = 'MS Sans Serif'
   Font.Style = []
+  Menu = MainMenu1
   OldCreateOrder = False
   Position = poDesktopCenter
   PixelsPerInch = 96
@@ -20,68 +21,23 @@ object Form1: TForm1
     Left = 472
     Top = 0
     Width = 321
-    Height = 329
+    Height = 321
+    Proportional = True
   end
-  object Button1: TButton
-    Left = 8
-    Top = 336
-    Width = 75
-    Height = 25
-    Caption = #1086#1090#1082#1088#1099#1090#1100
-    TabOrder = 0
-    OnClick = Button1Click
+  object Label1: TLabel
+    Left = 0
+    Top = 328
+    Width = 25
+    Height = 13
   end
   object Memo1: TMemo
     Left = 0
     Top = 0
     Width = 473
-    Height = 329
+    Height = 321
     ReadOnly = True
     ScrollBars = ssBoth
-    TabOrder = 1
-  end
-  object Button2: TButton
-    Left = 88
-    Top = 336
-    Width = 75
-    Height = 25
-    Caption = 'BGR565'
-    TabOrder = 2
-    OnClick = Button2Click
-  end
-  object Button3: TButton
-    Left = 168
-    Top = 336
-    Width = 75
-    Height = 25
-    Caption = 'RGB565'
-    TabOrder = 3
-    OnClick = Button3Click
-  end
-  object Button4: TButton
-    Left = 248
-    Top = 336
-    Width = 75
-    Height = 25
-    Caption = #1082#1086#1087#1080#1088#1086#1074#1072#1090#1100
-    TabOrder = 4
-    OnClick = Button4Click
-  end
-  object Button5: TButton
-    Left = 712
-    Top = 336
-    Width = 75
-    Height = 25
-    Caption = #1062#1074#1077#1090
-    TabOrder = 5
-    OnClick = Button5Click
-  end
-  object OpenDialog1: TOpenDialog
-    Filter = 
-      'All (*.jpg;*.jpeg;*.bmp;*.ico;*.emf;*.wmf)|*.jpg;*.jpeg;*.bmp;*.' +
-      'ico;*.emf;*.wmf'
-    Left = 16
-    Top = 24
+    TabOrder = 0
   end
   object XPManifest1: TXPManifest
     Left = 48
@@ -90,5 +46,76 @@ object Form1: TForm1
   object ColorDialog1: TColorDialog
     Left = 80
     Top = 24
+  end
+  object OpenPictureDialog1: TOpenPictureDialog
+    Left = 16
+    Top = 24
+  end
+  object SaveDialog1: TSaveDialog
+    Left = 112
+    Top = 24
+  end
+  object MainMenu1: TMainMenu
+    Left = 144
+    Top = 24
+    object Afqk1: TMenuItem
+      Caption = #1060#1072#1081#1083
+      OnClick = Afqk1Click
+      object N1: TMenuItem
+        Caption = #1054#1090#1082#1088#1099#1090#1100
+        OnClick = N1Click
+      end
+      object N2: TMenuItem
+        Caption = '-'
+      end
+      object N3: TMenuItem
+        Caption = #1057#1086#1093#1088#1072#1085#1080#1090#1100
+        Enabled = False
+        OnClick = N3Click
+      end
+      object N9: TMenuItem
+        Caption = '-'
+      end
+      object N10: TMenuItem
+        Caption = #1042#1099#1093#1086#1076
+        OnClick = N10Click
+      end
+    end
+    object N4: TMenuItem
+      Caption = #1055#1088#1072#1074#1082#1072
+      object N6: TMenuItem
+        Caption = #1050#1086#1087#1080#1088#1086#1074#1072#1090#1100
+        OnClick = N6Click
+      end
+      object N7: TMenuItem
+        Caption = #1054#1095#1080#1089#1090#1080#1090#1100
+        OnClick = N7Click
+      end
+    end
+    object N5: TMenuItem
+      Caption = #1050#1086#1085#1074#1077#1088#1090#1080#1088#1086#1074#1072#1090#1100
+      object BGR5651: TMenuItem
+        Caption = 'BGR565'
+        OnClick = BGR5651Click
+      end
+      object RGB5651: TMenuItem
+        Caption = '-'
+      end
+      object RGB5652: TMenuItem
+        Caption = 'RGB565'
+        OnClick = RGB5652Click
+      end
+      object N11: TMenuItem
+        Caption = '-'
+      end
+      object N12: TMenuItem
+        Caption = #1062#1074#1077#1090
+        OnClick = N12Click
+      end
+    end
+    object N8: TMenuItem
+      Caption = #1054' '#1087#1088#1086#1075#1088#1072#1084#1084#1077
+      OnClick = N8Click
+    end
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,25 +4,44 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ExtCtrls, ClipBrd, XPMan;
+  Dialogs, StdCtrls, ExtCtrls, ClipBrd, XPMan, Jpeg, ExtDlgs, Menus;
 
 type
   TForm1 = class(TForm)
-    Button1: TButton;
     Memo1: TMemo;
-    Button2: TButton;
-    OpenDialog1: TOpenDialog;
     Image1: TImage;
-    Button3: TButton;
-    Button4: TButton;
     XPManifest1: TXPManifest;
     ColorDialog1: TColorDialog;
-    Button5: TButton;
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
-    procedure Button3Click(Sender: TObject);
-    procedure Button4Click(Sender: TObject);
-    procedure Button5Click(Sender: TObject);
+    OpenPictureDialog1: TOpenPictureDialog;
+    Label1: TLabel;
+    SaveDialog1: TSaveDialog;
+    MainMenu1: TMainMenu;
+    Afqk1: TMenuItem;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    BGR5651: TMenuItem;
+    RGB5651: TMenuItem;
+    N6: TMenuItem;
+    N7: TMenuItem;
+    RGB5652: TMenuItem;
+    N8: TMenuItem;
+    N9: TMenuItem;
+    N10: TMenuItem;
+    N11: TMenuItem;
+    N12: TMenuItem;
+    procedure N1Click(Sender: TObject);
+    procedure N6Click(Sender: TObject);
+    procedure N7Click(Sender: TObject);
+    procedure N10Click(Sender: TObject);
+    procedure RGB5652Click(Sender: TObject);
+    procedure BGR5651Click(Sender: TObject);
+    procedure N12Click(Sender: TObject);
+    procedure Afqk1Click(Sender: TObject);
+    procedure N8Click(Sender: TObject);
+    procedure N3Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -31,22 +50,71 @@ type
 
 var
   Form1: TForm1;
+  stat: string;
 
 implementation
 
 {$R *.dfm}
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.N1Click(Sender: TObject);
+var bp:TBitmap;
+size: integer;
 begin
-if opendialog1.Execute then
-image1.Picture.LoadFromFile(opendialog1.FileName);
+if openpicturedialog1.Execute then
+memo1.Clear;
+image1.Picture.LoadFromFile(openpicturedialog1.FileName);
+bp:=TBitmap.Create;
+bp.Assign(image1.picture.Graphic);
+image1.picture.Bitmap.Assign(bp);
+size:= image1.Picture.Height*image1.Picture.Width*2;
+label1.Caption:=inttostr(image1.picture.height)+'x'+inttostr(image1.picture.Width)+' = '+ inttostr(size)+' Байт ('+floattostrf(size/1024, ffFixed, 4, 2)+' КБайт)';
 end;
 
-procedure TForm1.Button2Click(Sender: TObject);
+procedure TForm1.N6Click(Sender: TObject);
+begin
+memo1.SelectAll;
+memo1.CopyToClipboard;
+end;
+
+procedure TForm1.N7Click(Sender: TObject);
+begin
+memo1.Clear;
+end;
+
+procedure TForm1.N10Click(Sender: TObject);
+begin
+application.Terminate;
+end;
+
+procedure TForm1.RGB5652Click(Sender: TObject);
+var r,g,b, i, n, rgb565: integer;
+c:LongInt;
+s: string;
+begin
+stat:='rgb';
+memo1.Clear;
+for n:=0 to image1.Picture.height-1 do
+  begin
+  for i:=0 to image1.Picture.width-1 do
+    begin
+    c:=ColorToRGB(image1.Canvas.Pixels[i, n]);
+    r:=round(GetRValue(c)*31/255);
+    g:=round(GetGValue(c)*63/255);
+    b:=round(GetBValue(c)*31/255);
+    rgb565:=(r shl 11)or(g shl 5)or(b);
+    s:=s+'0x'+inttohex(hi(rgb565),1)+', 0x'+inttohex(lo(rgb565),1)+', ';
+    end;
+    memo1.Lines.Add(s);
+    s:='';
+  end;
+end;
+
+procedure TForm1.BGR5651Click(Sender: TObject);
 var r,g,b, i, n, bgr565: integer;
 c:LongInt;
 s: string;
 begin
+stat:='bgr';
 memo1.Clear;
 for n:=0 to image1.Picture.height-1 do
   begin
@@ -64,38 +132,9 @@ for n:=0 to image1.Picture.height-1 do
   end;
 end;
 
-procedure TForm1.Button3Click(Sender: TObject);
-var r,g,b, i, n, rgb565: integer;
+procedure TForm1.N12Click(Sender: TObject);
+var r,g,b, rgb565, bgr565: integer;
 c:LongInt;
-s: string;
-begin
-memo1.Clear;
-for n:=0 to image1.Picture.height-1 do
-  begin
-  for i:=0 to image1.Picture.width-1 do
-    begin
-    c:=ColorToRGB(image1.Picture.Bitmap.Canvas.Pixels[i, n]);
-    r:=round(GetRValue(c)*31/255);
-    g:=round(GetGValue(c)*63/255);
-    b:=round(GetBValue(c)*31/255);
-    rgb565:=(r shl 11)or(g shl 5)or(b);
-    s:=s+'0x'+inttohex(hi(rgb565),1)+', 0x'+inttohex(lo(rgb565),1)+', ';
-    end;
-    memo1.Lines.Add(s);
-    s:='';
-  end;
-end;
-
-procedure TForm1.Button4Click(Sender: TObject);
-begin
-memo1.SelectAll;
-memo1.CopyToClipboard;
-end;
-
-procedure TForm1.Button5Click(Sender: TObject);
-var r,g,b, i, n, rgb565, bgr565: integer;
-c:LongInt;
-s: string;
 begin
 if colordialog1.Execute then
     begin
@@ -108,6 +147,49 @@ if colordialog1.Execute then
     memo1.Text:='color rgb565: 0x'+inttohex(rgb565,1);
     memo1.lines.add('color bgr565: 0x'+inttohex(bgr565,1));
     end;
+end;
+
+procedure TForm1.Afqk1Click(Sender: TObject);
+begin
+if stat='' then
+N3.Enabled:=false;
+if (stat='bgr') or (stat='rgb') then
+N3.Enabled:=true;
+end;
+
+procedure TForm1.N8Click(Sender: TObject);
+begin
+showmessage('by Dmitriy Ivanov 2023');
+end;
+
+procedure TForm1.N3Click(Sender: TObject);
+var
+  byte1, byte2: byte;
+  fs: TFileStream;
+  r,g,b, i, n, bgr565: integer;
+c:LongInt;
+begin
+if savedialog1.Execute then
+fs := TFileStream.Create(savedialog1.FileName, fmCreate or fmOpenReadWrite or fmShareDenyWrite);
+fs.Seek(0, soFromBeginning);
+for n:=0 to image1.Picture.height-1 do
+  begin
+  for i:=0 to image1.Picture.width-1 do
+    begin
+    c:=ColorToRGB(image1.Picture.Bitmap.Canvas.Pixels[i, n]);
+    r:=round(GetRValue(c)*31/255);
+    g:=round(GetGValue(c)*63/255);
+    b:=round(GetBValue(c)*31/255);
+    bgr565:=(b shl 11)or(g shl 5)or(r);
+    byte1:=hi(bgr565);
+    byte2:=lo(bgr565);
+    fs.Write(byte1,sizeof(byte1));
+    fs.Write(byte2,sizeof(byte2));
+    end;
+  end;
+
+  fs.Free;
+
 end;
 
 end.
